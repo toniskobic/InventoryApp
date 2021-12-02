dd277b9778dec043e12b229c6a6ae15925793422
dznidaric <61658892+dznidaric@users.noreply.github.com>
2021-12-02 21:38:04 +0100

    resolve merge conflict

--- inv_app/lib/main.dart
+++ inv_app/lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'pocetna_stranica.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-     
-      debugShowCheckedModeBanner:false,
-      home: HomepageWidget(),
-        );
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Registration(),
+    );
   }
 }
-
-
