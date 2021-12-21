d45b07a6d23e1cfc3fe5d035245537c7ff3c8d41
tskobic <tskobic@foi.hr>
2021-12-22 00:56:12 +0100

    Strapi QR: - qr generate after resource entry implemented

--- api/resource/models/resource.js
+++ api/resource/models/resource.js
@@ -1,9 +1,6 @@
 "use strict";
-var qrCode = require("qrcode");
-var qr = require("qr-image");
-var FormData = require('form-data');
-var form = new FormData();
-var fs = require("fs");
+const qr = require("qr-image");
+const fs = require("fs");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
@@ -13,36 +10,25 @@ var fs = require("fs");
 module.exports = {
   lifecycles: {
     async beforeCreate(data) {
+      let qr_svg = qr.image(JSON.stringify(data), { type: "svg" });
+      qr_svg.pipe(fs.createWriteStream(".tmp/qr.svg"));
+    },
 
-    // Ovaj dio generira qr kod, ovdje koristim drugi qr npm package
-    //   data.name = "Some fixed name";
-
-    //   var qr_svg = qr.image(JSON.stringify(data), { type: "svg" });
-
-    //   const files = qr_svg.pipe(
-    //     fs.createWriteStream("qr.svg")
-    //   );
-
-
-    //Tu sam pokušavao sa pozivanjem kontrolera za upload
-    //   const body = { name: "qr", tag: "1", path: "i_love_qr.svg" };
-
-    //   const uploadedFiles = await strapi.plugins.upload.controllers.upload({
-    //     data: body,
-    //     files: files,
-    //   });
-
-
-    //Ovdje sam pokušavao sa formdata poslat zahtjev za upload
-    //   form.append("files", fs.createReadStream("i_love_qr.svg"));
-    //   form.append(
-    //     "data",
-    //     JSON.stringify({
-    //       alternativeText: "example",
-    //     })
-    //   );
-    //   form.submit("http://localhost:1337/upload", (err, res) => {
-    //   });
+    async afterCreate(result) {
+      const fileStat = await fs.statSync(".tmp/qr.svg");
+      const qr = await strapi.plugins.upload.services.upload.upload({
+        data: {
+          refId: result.id,
+          ref: "resource",
+          field: "qr",
+        },
+        files: {
+          path: ".tmp/qr.svg",
+          name: `qr${result.id}.svg`,
+          type: "image/svg+xml", // mime type
+          size: fileStat.size,
+        },
+      });
     },
   },
 };
