5c6c29ab6979baf75440cea51fb398632b524d8b
andelatunjic <atunjic@foi.hr>
2022-02-18 00:30:48 +0100

    Popravljanje ikone na loginu za vidljivost lozinke

--- inv_app/lib/Views/Forms/login.dart
+++ inv_app/lib/Views/Forms/login.dart
@@ -14,7 +14,7 @@ class LoginForm extends StatefulWidget {
 class LoginFormState extends State<LoginForm> {
   final _formKey = GlobalKey<FormState>();
 
-  bool passwordHidden = true;
+  bool passwordHidden = false;
 
   User user = User();
   void _logIn() {
