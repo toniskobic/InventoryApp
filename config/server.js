b02d9ee4d0594c6464d077319ab24b3eb0556412
tskobic <toni.skobic@gmail.com>
2023-07-03 15:24:23 +0200

    minor change

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '***REMOVED***'),
+      secret: env('ADMIN_JWT_SECRET', ''),
     },
     watchIgnoreFiles: [
       '**/AIR2108/qr.svg**',
--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '***REMOVED***'
+  jwtSecret: process.env.JWT_SECRET || ''
 };
